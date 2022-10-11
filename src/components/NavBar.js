2bbdd7b56ec83b006b87b48d3d50ee52b6d8eb04
Kevin Gicheha <gichehakevin@gmail.com>
2022-10-11 10:34:47 -0700

    with navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import {
+  CssBaseline,
+  AppBar,
+  Box,
+  Container,
+  Typography,
+  Toolbar,
+  IconButton,
+  MenuItem,
+  Button,
+  Tooltip,
+  Avatar,
+  Menu,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { styled } from "@mui/material";
+
+const NavBar = () => {
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const pages = ["About", "Skills", "Porfolio", "Blog", "Contact"];
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const CustomAppBar = styled(AppBar)({
+    backgroundColor: "white",
+  });
+
+  const CustomButton = styled(Button)({
+    color: "#FF014F",
+    borderColor: "#FF014F",
+    "&:hover": {
+      backgroundColor: "#FF0F59",
+      color: "#ffffff",
+      textDecoration: "underline",
+      borderColor: "#FF014F",
+    },
+  });
+
+  const CustomMenuButton = styled(Button)({
+    color: "#3C3E41",
+    display: "block",
+    "&:hover": {
+        textDecoration: "underline",
+      },
+
+  })
+  return (
+    <>
+      <CssBaseline />
+      <CustomAppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip>
+                <IconButton sx={{ p: 0 }}>
+                  <Avatar alt="logo" src={require("../Assets/logo.jpeg")} />
+                </IconButton>
+              </Tooltip>
+            </Box>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <CustomMenuButton
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2 }}
+                >
+                  {page}
+                </CustomMenuButton>
+              ))}
+            </Box>
+            <CustomButton variant="outlined">Resume</CustomButton>
+          </Toolbar>
+        </Container>
+      </CustomAppBar>
+    </>
+  );
+};
+
+export default NavBar;
